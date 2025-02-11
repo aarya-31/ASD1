dadeb88952d77b6bf4bfbe271967e78e88aafcbe
aarya-31 <154952329+aarya-31@users.noreply.github.com>
2025-02-11 09:22:03 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,4 +1,8 @@
 a=int(input("Eneter first number "))
 b=int(input("enter 2nd number "))
 c=a+b
-print("result = ",c)
+print("Addition = ",c)
+d=a-b
+print("Subtraction = ",d)
+print("Multiplication = ",a*b)
+print("Division = ",a/b)
